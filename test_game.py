2485a75b9f0a544b9a96ddf9098687445a6c3691
Paddy Cremen <cremenp@tcd.ie>
2018-05-08 15:10:00 +0000

    improve testing code readability

--- test_game.py
+++ test_game.py
@@ -11,7 +11,6 @@ class testGame(unittest.TestCase):
         test to check that after calling wipe_files, the length
         of the content string is 0
         """
-        
         text = "test text"
         
         run.add_incorrect_text(text)
@@ -51,17 +50,14 @@ class testGame(unittest.TestCase):
         test to check that answer.txt is not wiped if
         True is entred as an argument into wipe_game_test()
         """
-        
         sample_text = "sample text"
         
         run.wipe_game_text()
         
         with open("active-game-files/answer.txt", "a") as f:
             f.writelines(sample_text) 
-        
-        
+            
         run.wipe_game_text(True)
-        
         round_text = run.get_round_text()
         answer_text = "".join(round_text["answer text"])
         self.assertEqual(answer_text, sample_text)
@@ -72,7 +68,6 @@ class testGame(unittest.TestCase):
         test to check that the data entered into dump data will 
         be the same as the value returned by get_json_data
         """
-        
         data_to_dump = [{
             "should be 1": 1,
             "should be false": False,
@@ -83,7 +78,6 @@ class testGame(unittest.TestCase):
             
         run.dump_data(data_to_dump)
         
-        
         retrieved_data = run.get_json_data()
         retrived_dictionary = retrieved_data[0]
         
@@ -92,17 +86,12 @@ class testGame(unittest.TestCase):
         self.assertEqual(retrived_dictionary["should be false"], False)
         self.assertEqual(retrived_dictionary["should be 'test'"], "test")
         
-        
-        
-        
 
-        
     def test_col_size_returns_int_that_24_is_devisable_by(self):
         """
         test to check that get_col_size and get_col_sm_size returns
         an int. 24 % that int should be 0
         """
-        
         list_length_1 = [1]
         list_length_3 = [0,1,2]
         
@@ -127,12 +116,9 @@ class testGame(unittest.TestCase):
         test to check that initialize_used_question() creates a list
         with exactly one entry
         """
-        
-        
         run.initialize_used_question()
         with open("active-game-files/used_questions.json", "r") as f:
             result = json.load(f)
-        
         self.assertTrue(type(result) is list)
         self.assertEqual(len(result), 1)
         
@@ -142,14 +128,10 @@ class testGame(unittest.TestCase):
         test to check that initialize_used_question() creates a list
         with an entry that is a dictionary and has a 'question' key
         """
-        
-        
         run.initialize_used_question()
         with open("active-game-files/used_questions.json", "r") as f:
             result = json.load(f)
             only_result = result[0]
-        
-    
         self.assertNotEqual(only_result["question"], None)
         
         
@@ -158,7 +140,6 @@ class testGame(unittest.TestCase):
         test to check that get_current_player won't return players with a turn of
         false
         """
-        
         one_true = [{"turn" : False}, {"turn": False}, {"turn": True}, {"turn": False}]
         run.dump_data(one_true)
         one_true_result = run.get_current_player()
@@ -176,7 +157,6 @@ class testGame(unittest.TestCase):
         boolean. Should be true if dictionary's 
         'last question correct' value == True
         """
-        
         first_test_value = "test"
         second_test_value = [1,2,3]
         should_return_false = {"last question correct": False}
@@ -222,8 +202,6 @@ class testGame(unittest.TestCase):
         the player entered as an argument. The player should be
         replaced in the dictionary, not appended
         """
-        
-        
         test_game_data = [
             {
                 "no": 10,
@@ -236,9 +214,7 @@ class testGame(unittest.TestCase):
             ]
             
         run.dump_data(test_game_data)
-        
         original_data = run.get_json_data()
-        
         replacement_player = {
             "no": 5,
             "test": "success!",
@@ -246,7 +222,6 @@ class testGame(unittest.TestCase):
         }
         
         run.return_player_to_game_data(replacement_player)
-        
         new_data = run.get_json_data()
         
         self.assertEqual(len(original_data), len(new_data))
@@ -255,14 +230,12 @@ class testGame(unittest.TestCase):
         self.assertTrue(new_data[1]["additional key"])
     
     
-    
     def test_ask_question_writes_current_player_question(self):
         """
         test to check that ask_question writes the appropriate 
         question tuple entry for the player with a turn value of
         True
         """
-        
         run.wipe_game_text()
         
         test_player_data =[{"previous": False, "last question correct": True, "no": 1, "incorrect guesses": "", "username": "Chris", "turn": False, "lives": 2, "question": ["This kind of coat can you put on only when it is wet. What is it?", "A coat of paint", "paint"], "answer": ["a", "paint", "of", "coat"], "score": 5}, {"previous": True, "last question correct": False, "no": 2, "incorrect guesses": "<br>the dictionary", "username": "Stewie", "turn": False, "lives": 1, "question": ["I am a word. If you pronounce me rightly, it will be wrong. If you pronounce me wrong it is right? What word am I?", "Wrong", "wrong"], "answer": ["the", "dictionary"], "score": 4}, {"previous": False, "last question correct": True, "no": 3, "incorrect guesses": "", "username": "Meg", "turn": True, "lives": 2, "question": ["It has Eighty-eight keys but can't open a single door? What is it?", "A piano", "piano"], "answer": "", "score": 4}]
@@ -278,10 +251,6 @@ class testGame(unittest.TestCase):
             
             
         self.assertEqual("".join(result), result_should_be)
-        
-        
-        
-        
         
         
     def test_link_first_picture_value(self):
@@ -314,7 +283,6 @@ class testGame(unittest.TestCase):
         test to check that a picture tuple list is returned if 
         picture is eneted as an argument into get_questions_answers_keywords
         """
-        
         returned_tuple_list = run.get_questions_answers_keywords("Picture")
         first_entry = returned_tuple_list[0]
         self.assertEqual(len(first_entry), 4)
@@ -326,7 +294,6 @@ class testGame(unittest.TestCase):
         test to check that add_host_text adds the entered
         text into host.txt
         """
-        
         run.wipe_game_text()
         test_text = "test text"
         
@@ -344,18 +311,14 @@ class testGame(unittest.TestCase):
         the incorrect guesses of the player entered as an 
         argument. Guesses should be written to incorrect_guesses.txt
         """
-        
         test_player = {"username": "test", "incorrect guesses" : "<br>a wrong answer<br>another incorrect answer"}
-        
         result_should_be = "<span id='guesses-title'>Incorrect Guesses: </span> <span class='guesses'> <br>a wrong answer<br>another incorrect answer </span>"
         
         run.wipe_game_text()
-        
         run.add_incorrect_guesses_text(test_player)
         
         with open("active-game-files/incorrect_guesses.txt", "r") as f:
             result= f.readlines()
-            
             
         self.assertEqual("".join(result), result_should_be)
         
@@ -366,7 +329,6 @@ class testGame(unittest.TestCase):
         the player with turn: True has turn changed to False and
         previous turned to true
         """
-        
         test_players = [{"username": "p1", "turn": False, "previous": True}, {"username": "p2", "turn": True, "previous": False}, {"username": "p3", "turn": False, "previous": False}]
         
         run.dump_data(test_players)
@@ -388,7 +350,6 @@ class testGame(unittest.TestCase):
         to index 0 when and only when the final player in the list has
         a turn value of True
         """
-        
         should_not_be_index_0_turn =[{"username": "p1", "turn": False, "previous": True}, {"username": "p2", "turn": True, "previous": False}, {"username": "p3", "turn": False, "previous": False}, {"username": "p4", "turn": False, "previous": False}]
         
         run.dump_data(should_not_be_index_0_turn)
@@ -412,7 +373,6 @@ class testGame(unittest.TestCase):
         file contains no previous player. Otherwise should return the player with a 
         previous value of True
         """
-        
         no_previous_player = [{"username": "p1", "turn": False, "previous": False}, {"username": "p2", "turn": True, "previous": False}, {"username": "p3", "turn": False, "previous": False}, {"username": "p4", "turn": False, "previous": False}]
         run.dump_data(no_previous_player)
         previous_player = run.get_previous_player()
@@ -430,7 +390,6 @@ class testGame(unittest.TestCase):
         'Correct!' to correct.txt if the players answer contains 
         the questions keyword. If it does not, it should write 'incorrect'
         """
-        
         run.wipe_game_text()
         should_write_incorrect = [{"question": ["What starts with a 'P', ends with an 'E'and has thousands of letters?", "The Post Office", "post"],  "answer": ["wrong", "answer"],  "previous": True, "incorrect guesses": "", "no": 1}]
         run.dump_data(should_write_incorrect)
@@ -459,7 +418,6 @@ class testGame(unittest.TestCase):
         as an argument. If false is entered, it should 
         subtract 1 from the previous player's life
         """
-        
         test_data = [{"previous": True, "lives":0, "score":0, "no":1}]
         run.dump_data(test_data)
         
@@ -473,11 +431,9 @@ class testGame(unittest.TestCase):
             run.update_lives_and_score(False)
         
         result= run.get_json_data()
-        
+
         self.assertEqual(result[0]["score"], to_add_to_score)
         self.assertEqual(result[0]["lives"], (- to_subtract_from_lives))
-        
-        
         
         
     def test_players_with_0_lives_are_eliminated(self):
@@ -485,14 +441,12 @@ class testGame(unittest.TestCase):
         test to check that eliminate_dead_players will remove a
         player with 0 lives
         """
-        
         no_dead_players = [{"no": 1, "lives" : 2, "username": "test"},{"no": 2, "lives" : 1, "username": "test"}, {"no": 3, "lives" : 6, "username": "test"}]
         
         run.dump_data(no_dead_players)
         run.eliminate_dead_players()
         result = run.get_json_data()
         self.assertEqual(len(no_dead_players), len(result))
-        
         
         one_dead_player = [{"no": 1, "lives" : 2, "username": "test"},{"no": 2, "question": ("", "", "", ""), "score": 2, "lives" : 0, "username": "test"}, {"no": 3, "lives" : 6, "username": "test"}]
         
@@ -509,7 +463,6 @@ class testGame(unittest.TestCase):
         test to check that add_eliminated_text correctly identifies
         the username of the player that was eliminated
         """
-        
         run.wipe_game_text
         test_game_data = [{"no": 1, "lives" : 2, "username": "wrong name"},{"no": 2, "question": ("", "", "", ""), "score": 2, "lives" : 0, "username": "success"}, {"no": 3, "lives" : 6, "username": "not this player"}]
         run.dump_data(test_game_data)
@@ -528,12 +481,11 @@ class testGame(unittest.TestCase):
         dictionaries. Each dictionary should have a username and score 
         value only
         """
-        
         returned_list = run.get_leaderboard_data()
         
         for dictionary in returned_list:
             self.assertTrue(dictionary["username"])
-            self.assertTrue(dictionary["score"])
+            self.assertTrue(dictionary["score"] >=0)
             self.assertEqual(len(dictionary), 2)
         
     
@@ -542,7 +494,6 @@ class testGame(unittest.TestCase):
         test to check that post_leaderboard_data adds its argument
         to high_scores.json, wipes other data
         """
-        
         orginal_leaderboard_data = run.get_leaderboard_data()
         length_of_original_leaderboard_data = len(orginal_leaderboard_data)
         
@@ -562,7 +513,6 @@ class testGame(unittest.TestCase):
         test to check that all_to_leaderboard appends, but doesn't
         wipe, high_scores.json
         """
-        
         orginal_leaderboard_data = run.get_leaderboard_data()
         length_of_original_leaderboard_data = len(orginal_leaderboard_data)
         
@@ -586,7 +536,6 @@ class testGame(unittest.TestCase):
         test to check that add_correct_answer_text correctly posts the answer of the 
         argument player's question for both picture and text questions
         """
-        
         run.wipe_game_text()
         
         text_question_plyaer = {"question": ["I have rivers, but do not have water. I have dense forests, but no trees and animals. I have cities, but no people live in those cities. What am I?", "A map", "map"]}
@@ -611,7 +560,6 @@ class testGame(unittest.TestCase):
         boolean. Should be False if players.json is not 
         empty. Otherwise should be True
         """
-        
         result = run.all_players_gone()
         self.assertEqual(type(result), bool)
         
@@ -624,25 +572,13 @@ class testGame(unittest.TestCase):
         self.assertFalse(run.all_players_gone())
         
         
-    
-        
     def test_get_soted_scores_returns_list_of_objects_sorted_by_descending_score(self):
         """
         test to check that get_sorted_scores returns a list of object where each 
         object's score value is greater than or equal to the following object
         """
-        
         sorted_scores = run.get_sorted_scores()
         
         for i in range(len(sorted_scores)-1):
             self.assertTrue(sorted_scores[i]["score"] >= sorted_scores[i+1]["score"])
-        
-    
-        
-        
-    
-        
-        
-        
-        
         
