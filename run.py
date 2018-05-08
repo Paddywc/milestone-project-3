db3706d3bc9c2703e5edcb9567092eada0f43d02
Paddy Cremen <cremenp@tcd.ie>
2018-05-08 15:02:58 +0000

    reorder files

--- run.py
+++ run.py
@@ -8,6 +8,12 @@ app = Flask(__name__)
 
 app.secret_key = 'some_secret'
 
+
+"""
+functions for working with game text files
+"""
+
+
 def wipe_game_text(game_over=False):
     """
     wips all game text files
@@ -171,6 +177,10 @@ def get_round_text():
     return round_text_dictionary
 
 
+"""
+functions for working with json files
+"""
+
 def dump_data(player_list):
     """
     adds argument to players.json. Wipes pre-existing content
@@ -246,6 +256,171 @@ def get_current_player():
             current_player = player
 
     return current_player
+    
+    
+
+def get_used_questions():
+    """
+    returns list of question tuples from used_questions.json
+    """
+    with open("active-game-files/used_questions.json", "r") as f:
+        used_questions = json.load(f)
+        return used_questions
+
+
+def initialize_used_question():
+    """
+    wipes used_questions.json and replaces content 
+    with a list with one dictionary. Dictionary 
+    must have 'question' key for other functions 
+    to work
+    """
+    question_list = []
+    question_list.append({"question": "sample_question"})
+
+    with open("active-game-files/used_questions.json", mode="w", encoding="utf-8") as f:
+        json.dump(question_list, f)
+
+
+def add_to_used_questions(question, used_questions):
+    """
+    appends question to used_questions, then replaces
+    used.question.json contents with appended used_questions
+    """
+    used_questions.append({"question": question[0]})
+
+    with open("active-game-files/used_questions.json", mode="w", encoding="utf-8") as f:
+        json.dump(used_questions, f)
+        
+
+def get_previous_player():
+    """
+    returns the dictionary in players.json with a 
+    'previous' value of True
+    """
+    game_data = get_json_data()
+    previous_player = {}
+
+    for player in game_data:
+        if player["previous"] == True:
+            previous_player = player
+
+    return previous_player
+
+
+def set_new_chosen_and_previous_player():
+    """
+    player in players.json with a 'turn' value 
+    of True replaced with False but 'previous' 
+    set to true. Next player in list (or first player in 
+    list if at end of list) has turn set to True. 
+    All other players have turn and previous set to False
+    """
+    game_data = get_json_data()
+    number_of_players = len(game_data)
+    player_index = 0
+
+    for player in game_data:
+        player["previous"] = False
+
+    for player in game_data:
+        if player["turn"] == True:
+            player["previous"] = True
+            player_index = game_data.index(player)
+            player["turn"] = False
+
+    if player_index < number_of_players-1:
+        game_data[player_index + 1]["turn"] = True
+
+    else:
+        game_data[0]["turn"] = True
+
+    dump_data(game_data)
+
+def eliminate_dead_players():
+    """
+    removes player from players.json if their lives value
+    is 0. If player removed, adds text saying that player has 
+    been removed. Also adds text displaying the correct answer to
+    their question
+    """
+    game_data = get_json_data()
+    player_index = 999
+    eliminated_player = {}
+
+    for player in game_data:
+        if player["lives"] <= 0:
+            add_to_leaderboard(player)
+            player_index = game_data.index(player)
+            add_eliminated_text(player)
+            add_correct_answer_text(player)
+
+            eliminated_player = player
+
+    if player_index != 999:
+        game_data.pop(player_index)
+        dump_data(game_data)
+
+    dump_data(game_data)
+    return game_data
+    
+    
+def add_to_leaderboard(player):
+    """
+    appends high_scores.json with the argument player's
+    username and score
+    """
+    username = player["username"]
+    score = player["score"]
+
+    saved_score = {"username": username, "score": score}
+    leaderboard_data = get_leaderboard_data()
+    leaderboard_data.append(saved_score)
+
+    post_leaderboard_data(leaderboard_data)
+
+
+def create_game_data(number_of_players):
+    """
+    initializes players.json file with a list
+    containing a dictionary for each player
+    """
+    player_list = []
+    for i in range(number_of_players):
+        username = request.form["player-{0}-username".format(i+1)]
+        player_object = {
+            "username": username,
+            "lives": 1,
+            "score": 0,
+            "question": "",
+            "last question correct": True, #determines if a new question is set. Therefore initially set to True
+            "turn": False, #first player in list will have this changed to True
+            "previous": False,
+            "answer": "",
+            "no": i+1, #a unique number for identifying each player in the game. Also used for card styling in game.html
+            "incorrect guesses": ""
+        }
+        player_list.append(player_object)
+    dump_data(player_list)
+
+
+def all_players_gone():
+    """
+    returns True if players.json list empty. Otherwise
+    returns False
+    """
+    game_data = get_json_data()
+
+    if len(game_data) > 0:
+        return False
+    else:
+        return True
+
+
+
+"""
+functions for setting usernames 
+"""
 
 
 def set_username():
@@ -262,7 +437,9 @@ def set_username():
         print("")
         set_username()
 
-
+"""
+functions to rending templates 
+"""
 
 def get_col_size(game_data):
     """
@@ -287,6 +464,10 @@ def get_col_sm_size(game_data):
     col_size = int(12/players)
 
     return col_size
+    
+"""
+functions for gameplay loop (that are not directly related to JSON files)
+"""
 
 
 def set_difficulty(current_score):
@@ -342,40 +523,6 @@ def get_questions_answers_keywords(difficulty):
             tuples_list.append((doc_lines[i], doc_lines[i+1], doc_lines[i+2]))
 
         return tuples_list
-
-
-def get_used_questions():
-    """
-    returns list of question tuples from used_questions.json
-    """
-    with open("active-game-files/used_questions.json", "r") as f:
-        used_questions = json.load(f)
-        return used_questions
-
-
-def initialize_used_question():
-    """
-    wipes used_questions.json and replaces content 
-    with a list with one dictionary. Dictionary 
-    must have 'question' key for other functions 
-    to work
-    """
-    question_list = []
-    question_list.append({"question": "sample_question"})
-
-    with open("active-game-files/used_questions.json", mode="w", encoding="utf-8") as f:
-        json.dump(question_list, f)
-
-
-def add_to_used_questions(question, used_questions):
-    """
-    appends question to used_questions, then replaces
-    used.question.json contents with appended used_questions
-    """
-    used_questions.append({"question": question[0]})
-
-    with open("active-game-files/used_questions.json", mode="w", encoding="utf-8") as f:
-        json.dump(used_questions, f)
 
 
 def check_question_is_original(question_tuple):
@@ -456,56 +603,9 @@ def ask_question():
         add_question_text(question[0])
 
 
-
-def get_previous_player():
-    """
-    returns the dictionary in players.json with a 
-    'previous' value of True
-    """
-    game_data = get_json_data()
-    previous_player = {}
-
-    for player in game_data:
-        if player["previous"] == True:
-            previous_player = player
-
-    return previous_player
-
-
-def set_new_chosen_and_previous_player():
-    """
-    player in players.json with a 'turn' value 
-    of True replaced with False but 'previous' 
-    set to true. Next player in list (or first player in 
-    list if at end of list) has turn set to True. 
-    All other players have turn and previous set to False
-    """
-    game_data = get_json_data()
-    number_of_players = len(game_data)
-    player_index = 0
-
-    for player in game_data:
-        player["previous"] = False
-
-    for player in game_data:
-        if player["turn"] == True:
-            player["previous"] = True
-            player_index = game_data.index(player)
-            player["turn"] = False
-
-    if player_index < number_of_players-1:
-        game_data[player_index + 1]["turn"] = True
-
-    else:
-        game_data[0]["turn"] = True
-
-    dump_data(game_data)
-
-
-
 def select_player(game_data):
     """
-    returns player from players.json with a 'Turn'
+    returns player from argument with a 'Turn'
     value of True
     """
     chosen_player = {}
@@ -625,87 +725,9 @@ def update_lives_and_score(correct):
 
     return_player_to_game_data(previous_player)
 
-
-
-def eliminate_dead_players():
-    """
-    removes player from players.json if their lives value
-    is 0. If player removed, adds text saying that player has 
-    been removed. Also adds text displaying the correct answer to
-    their question
-    """
-    game_data = get_json_data()
-    player_index = 999
-    eliminated_player = {}
-
-    for player in game_data:
-        if player["lives"] <= 0:
-            add_to_leaderboard(player)
-            player_index = game_data.index(player)
-            add_eliminated_text(player)
-            add_correct_answer_text(player)
-
-            eliminated_player = player
-
-    if player_index != 999:
-        game_data.pop(player_index)
-        dump_data(game_data)
-
-    dump_data(game_data)
-    return game_data
-
-
-def add_to_leaderboard(player):
-    """
-    appends high_scores.json with the argument player's
-    username and score
-    """
-    username = player["username"]
-    score = player["score"]
-
-    saved_score = {"username": username, "score": score}
-    leaderboard_data = get_leaderboard_data()
-    leaderboard_data.append(saved_score)
-
-    post_leaderboard_data(leaderboard_data)
-
-
-def create_game_data(number_of_players):
-    """
-    initializes players.json file with a list
-    containing a dictionary for each player
-    """
-    player_list = []
-    for i in range(number_of_players):
-        username = request.form["player-{0}-username".format(i+1)]
-        player_object = {
-            "username": username,
-            "lives": 1,
-            "score": 0,
-            "question": "",
-            "last question correct": True, #determines if a new question is set. Therefore initially set to True
-            "turn": False, #first player in list will have this changed to True
-            "previous": False,
-            "answer": "",
-            "no": i+1, #a unique number for identifying each player in the game. Also used for card styling in game.html
-            "incorrect guesses": ""
-        }
-        player_list.append(player_object)
-    dump_data(player_list)
-
-
-def all_players_gone():
-    """
-    returns True if players.json list empty. Otherwise
-    returns False
-    """
-    game_data = get_json_data()
-
-    if len(game_data) > 0:
-        return False
-    else:
-        return True
-
+"""
+routing functions
+"""
 
 @app.route("/", methods=["GET"])
 def index():
@@ -797,8 +819,6 @@ def render_game():
     game_data = get_json_data()
 
     return render_template("game.html", correct_text="".join(round_text["correct text"]), eliminated_text="".join(round_text["eliminated text"]), answer_text="".join(round_text["answer text"]), host_text="".join(round_text["host text"]), question_text="".join(round_text["question text"]), incorrect_guesses_text="".join(round_text["incorrect guesses text"]),   col_size=col_size, col_sm_size=col_sm_size,  game_data=game_data)
-
-
 
 
 if __name__ == '__main__':
